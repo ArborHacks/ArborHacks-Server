365fda96a420734b0d65021250a23f09dfd2042b
Ubuntu <ubuntu@ip-172-31-89-72.ec2.internal>
2022-04-16 18:07:09 +0000

    added some stuff

--- app.js
+++ app.js
@@ -1,34 +1,42 @@
 #!/usr/bin/env node
 const express = require('express');
-const sys = require('sys');
+const https = require('https');
 const exec = require('child_process').exec;
-const bodyParser = require('body-parser');
 const multer = require('multer');
+const cors = require('cors');
+const sys = require('sys');
 const fs = require('fs');
 
+// variable and function declarations
 const app = express();
 
-const hostname = '127.0.0.1';
-const port = 3005;
+app.use(cors());
 
-var storage =   multer.diskStorage({
+const port = 3000;
+
+var ssl_certs = {
+	key: fs.readFileSync('keys/key.pem'),
+	cert: fs.readFileSync('keys/cert.pem')
+};
+
+var storage = multer.diskStorage({
   destination: function (req, file, callback) {
-    console.log(req.body);
     callback(null, './uploads');
   },
   filename: function (req, file, callback) {
-    callback(null,  file.originalname.substring(0,file.originalname.lastIndexOf('.')) + '-' + Date.now() + file.originalname.substring(file.originalname.lastIndexOf('.'),file.originalname.length));
+    callback(null, file.originalname.substring(0,file.originalname.lastIndexOf('.')) + '-' + Date.now() + file.originalname.substring(file.originalname.lastIndexOf('.'),file.originalname.length));
   }
 });
 
 var upload = multer({ storage: storage }).single('foo');
 
-app.get('/', function(req, res) {
+// routes
+app.get('/api', function(req, res) {
     res.send('Hello World!')
 });
 
-app.get('/run', function(req, res) {
-    exec("make test", function(err, stdout, stderr) {
+app.get('/api/run', function(req, res) {
+    exec("ls", function(err, stdout, stderr) {
         if(err) console.error(err);
         else {
             console.log(`stdout: ${stdout}`);
@@ -38,12 +46,19 @@ app.get('/run', function(req, res) {
     });
 });
 
-app.post('/upload', function(req, res) {
+app.post('/api/upload', function(req, res) {
     upload(req, res, function(err) {
-        res.redirect('back');
+        exec("ls -l", function(err, stdout, stderr) {
+            if(err) console.error(err);
+            else {
+                res.send({'text': stdout});
+            }
+        });
     });
 });
 
-app.listen(port, function() {
-    console.log(`Example app listening on port ${port}`)
-});
+// app.listen(port, function() {
+    // console.log(`Example app listening on port ${port}`)
+// });
+
+https.createServer(ssl_certs, app).listen(port);
