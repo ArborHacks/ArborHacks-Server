332e074d36152aaf39feb437fe7eccf7b7311507
wlshen123 <wesleylshen@gmail.com>
2022-04-16 07:16:41 -0400

    added file upload

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
 #!/usr/bin/env node
-const http = require('http');
+const express = require('express');
+const sys = require('sys');
+const exec = require('child_process').exec;
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const fs = require('fs');
+
+const app = express();
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 3005;
 
-const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello World');
+var storage =   multer.diskStorage({
+  destination: function (req, file, callback) {
+    console.log(req.body);
+    callback(null, './uploads');
+  },
+  filename: function (req, file, callback) {
+    callback(null,  file.originalname.substring(0,file.originalname.lastIndexOf('.')) + '-' + Date.now() + file.originalname.substring(file.originalname.lastIndexOf('.'),file.originalname.length));
+  }
 });
 
-server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
+var upload = multer({ storage: storage }).single('foo');
+
+app.get('/', function(req, res) {
+    res.send('Hello World!')
+});
+
+app.get('/run', function(req, res) {
+    exec("make test", function(err, stdout, stderr) {
+        if(err) console.error(err);
+        else {
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+            res.send(`running? \n${stdout}`);
+        }
+    });
+});
+
+app.post('/upload', function(req, res) {
+    upload(req, res, function(err) {
+        res.redirect('back');
+    });
+});
+
+app.listen(port, function() {
+    console.log(`Example app listening on port ${port}`)
 });
